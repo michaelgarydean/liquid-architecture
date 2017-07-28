46e9dd607f8cbcc5600721fa83eb1efbef4afd1e
ehsan <e@ewerx.ca>
2017-07-28 15:28:39 -0400

    rendering lines extending from points WIP

---
+++ TouchDesigner/shaders/lines_render.fs.glsl
@@ -0,0 +1,57 @@
+// Liquid Architecture
+// Lines Rendering Fragment Shader
+// MAT shader for lines
+
+uniform sampler2D sColorRamp; // gradient for sound based coloring
+
+uniform float uSpotlightBlend;
+// each uSoundN vector stores xyz position of the sound + amplitude value
+uniform vec4 uSound1;
+
+in Vertex {
+	vec4 color;
+	vec3 camSpaceVert;
+	vec3 camVector;
+	vec3 norm;
+    //vec3 worldSpaceVert;
+    float soundDistance; // distance from particle to sound source in camera space
+} vVert;
+
+out vec4 fragColor;
+
+
+// ---------------------------------------------------------------------------
+
+// picks a color from the sColorRamp gradient using the distance as vertical texture coordinate
+// applies alpha using distance and magnitude (shorter distance = higher alpha)
+// TODO: distance is not scaled properly ?
+vec4 spotlightColor(float distance, float magnitude)
+{
+	vec4 rampColor = texture(sColorRamp, vec2(0.5, clamp(distance,0.0,1.0)));
+    rampColor.a = (1.0-distance) * magnitude;
+    return rampColor;
+}
+
+// picks a color from the sColorRamp gradient using the distance value ratio to cameras-space Z distance
+// creates a sort of sweep along Z-axis as input distance changes
+vec4 zDistanceRampColor(float distance)
+{
+    float rampV = (-vVert.camSpaceVert.z) / (distance*10.0);
+    vec4 rampColor = texture(sColorRamp, vec2(0.5, rampV));
+    rampColor.a = 1.0 - rampV;
+    return rampColor;
+}
+
+// ---------------------------------------------------------------------------
+void main()
+{
+	TDCheckDiscard(); // discard unused pixels
+
+    // apply spotlight effect based on sound position (camera space)
+    vec4 color = mix(vVert.color, spotlightColor(vVert.soundDistance, clamp(uSound1.w, 0.0, 1.0)), uSpotlightBlend);
+
+    // TODO: not sure this is useful at all
+    //vec4 color = mix(vVert.color, zDistanceRampColor(uSound1.w), uSpotlightBlend);
+
+	fragColor = TDOutputSwizzle(color);
+}
---
+++ TouchDesigner/shaders/lines_render.vs.glsl
@@ -0,0 +1,66 @@
+// Liquid Architecture
+// Lines Rendering Vertex Shader
+// MAT shader for lines
+
+// inputs
+uniform sampler2D sPositionMap;
+uniform sampler2D sHeightMap;
+uniform sampler2D sColorMap; // particle base colors
+uniform sampler2D sColorRamp; // gradient for sound based coloring
+uniform vec3 uResolution;
+uniform vec4 uSound1; // audio position (xyz) and value (w)
+
+// outputs
+out Vertex {
+	vec4 color;
+	vec3 camSpaceVert;
+	vec3 camVector;
+	vec3 norm;
+    float soundDistance;
+} vVert;
+
+// ---------------------------------------------------------------------------
+
+float distanceToPoint(vec3 point, vec4 fromPoint) {
+	vec3 pos = point;
+	pos.z = pos.z - 5.0; // HACK: why?
+	vec4 pointInWorldSpace = normalize(uTDMat.world * vec4(pos, 1.0));
+	//vec4 soundInCameraSpace = normalize(uTDMat.cam * soundInWorldSpace);
+	return distance(normalize(fromPoint), pointInWorldSpace);
+}
+
+// ---------------------------------------------------------------------------
+void main()
+{
+    float id = gl_InstanceID;
+
+    int rowIndex = int(floor(id / uResolution.x));
+    float u = ((id - (rowIndex * uResolution.x) + 0.5)) / uResolution.x;
+    float v = (rowIndex + 0.5) / uResolution.y;
+    vec2 mapCoord = vec2(u,v);
+	vec4 position = texture(sPositionMap, mapCoord);
+
+    vec4 worldSpaceVert = TDDeform(position);
+    vec4 camSpaceVert = uTDMat.cam * worldSpaceVert;
+    vec3 camSpaceNorm = uTDMat.camForNormals * TDDeformNorm(N).xyz;
+    vec3 camVec = -camSpaceVert.xyz;
+
+    // outputs for frag shader
+    vVert.norm.stp = camSpaceNorm.stp;
+    vVert.camSpaceVert.xyz = camSpaceVert.xyz;
+    vVert.camVector.stp = camVec.stp;
+    vVert.color = texture(sColorMap, mapCoord);
+
+    // distance from particle to sound source in camera space
+    vVert.soundDistance = distanceToPoint(uSound1.xyz, camSpaceVert);
+
+    // offset for second line vertex
+    float lineHeight = 0.0;
+    if (mod(gl_VertexID,2) == 1) {
+        lineHeight = texture(sHeightMap, mapCoord).x;
+    }
+    vec4 projSpaceVert = TDWorldToProj(worldSpaceVert);
+    projSpaceVert.y -= lineHeight;
+
+	gl_Position = projSpaceVert;
+}
