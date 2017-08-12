2e9b137e1d0130ba79910e1df0575e7bcf372488
Ehsan <e@ewerx.ca>
2017-08-12 01:31:25 -0700

    color switching (WIP) - rework lerp - add static pass thru shader - modify model switcher to include color maps - make example model+color switch network

--- TouchDesigner/shaders/static.fs.glsl
+++ TouchDesigner/shaders/static.fs.glsl
@@ -1,12 +1,9 @@
 // Liquid Architecture
-// Displacer Fragment Shader
-// TOP shader for displacing paticles along one axis
+// Static (no motion) Fragment Shader
+// TOP shader for interpolating values between initial and current
 
-uniform vec3 uShiftDir;
-uniform vec3 uSliceDir;
 uniform float uDelta;
 uniform float uReset;
-uniform float uThreshold;
 
 layout(location = 0) out vec4 outPos;
 layout(location = 1) out vec4 outVel;
@@ -25,19 +22,10 @@ void main()
     vec3 pos = texture(sTD2DInputs[0], vUV.st).rgb;
 
     if (uReset == 0.0) {
-        // to make it sweep from outside the model bounds
-        float x = dot(initialPos, uSliceDir) + 8.0;
-        float edge = uThreshold * 8.0;
-        // return 1 if x > edge, else 0
-        float step = step(edge, x);
         vec3 vel = texture(sTD2DInputs[1], vUV.st).rgb;
-        vec3 noise = texture(sTD2DInputs[3], vUV.st).rgb;
-        vel -= uShiftDir * abs(noise.x*140.0) * uDelta;
-        vel += noise * 0.001 * uDelta;
-        pos += vel * uDelta;
 
-        vel = mix(vel, vec3(0.0), step);
-        pos = mix(pos, initialPos, step);
+        vel = vel;
+        pos = pos;
 
         outPos = vec4(pos, 1.0);
         outVel = vec4(vel, 1.0);
